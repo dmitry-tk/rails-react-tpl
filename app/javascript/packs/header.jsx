874b8750b4fc49bfb2d9495009dbb9af7011259f
Dmitry <jesusinyourtown@gmail.com>
2017-11-15 13:03:49 +0200

    basic styles for header and footer, layout css included in commons, minor fixes

--- app/javascript/packs/header.jsx
+++ app/javascript/packs/header.jsx
@@ -1,14 +1,12 @@
 import React from 'react'
 
-
 export default class Header extends React.Component {
     render() {
         return (
           <div className="header">
             <div className="header-inner fl-cont fl-center-h">
-
                     <div className="fl-wrap fl-grow padd-left-md red">
-
+                        header
                     </div>
                     <div className="fl-wrap padd-ri-md hidden">
                 </div>
