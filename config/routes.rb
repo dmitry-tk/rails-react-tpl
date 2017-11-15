da76edd2cf31e2272eb1d673345c1a6c322fc322
Dmitry <jesusinyourtown@gmail.com>
2017-11-15 16:06:39 +0200

    root to index page

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
-  get 'home/index'
 
+  get 'home/index'
   get 'home/test'
 
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'home#index'
+
+  scope '/api' do
+    # todo
+  end
+
 end
